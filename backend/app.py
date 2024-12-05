c93c5f99e3cd391995960c869e2b4f4de6281a4b
zb0r0 <krystin52@gmail.com>
2024-12-05 21:59:43 +0100

    aktualizacja cors

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["http://192.168.18.2:8080", "http://48.209.24.37:8080"]}})
+CORS(app, resources={r"/*": {"origins": ["http://192.168.18.2:8080", "http://48.209.24.37:8080"
+                                         , "http://48.209.24.37:8081"]}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123123@48.209.24.37/sklep'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secretKey:D'

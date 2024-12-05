7a5844638e558f21dd9d9c185c033ba8537767c7
zb0r0 <krystin52@gmail.com>
2024-12-05 22:09:17 +0100

    aktualizacja cors i product grid

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["http://192.168.18.2:8080", "http://48.209.24.37:8080"
-                                         , "http://48.209.24.37:8081"]}})
+CORS(app, resources={r"/*": {"origins": ["*"]}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123123@48.209.24.37/sklep'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secretKey:D'

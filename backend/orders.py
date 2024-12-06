2b24dd64bb53b7c53f2092378b07538b6b040768
zb0r0 <krystin52@gmail.com>
2024-12-06 12:23:27 +0100

    http front

--- backend/orders.py
+++ backend/orders.py
@@ -64,7 +64,7 @@ def create_order():
                 "quantity": item['quantity']
             } for item in cart
         ],
-        "continueUrl": "https://48.209.24.37:8080/notify"
+        "continueUrl": "http://48.209.24.37:8080/notify"
     }
 
     # Pobranie tokenu dostępu PayU
